d45be02aec12ea9e561bed0e355fbc01e8f30085
mfish33 <32677537+mfish33@users.noreply.github.com>
2020-06-07 10:33:21 -0400

    navbar overlap fix

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {faBars} from '@fortawesome/free-solid-svg-icons'
 import {faUser} from '@fortawesome/free-solid-svg-icons'
+import { ContentfulService } from '../services/contentful.service';
+import { Course } from '../../shared/models/course'
 
 
 @Component({
@@ -12,10 +14,12 @@ export class NavbarComponent implements OnInit {
 
   faUser=faUser
   faBars=faBars
-  constructor() { }
+  courses:Promise<[string,Course][]>
+
+  constructor(private content:ContentfulService) { }
 
   ngOnInit() {
-
+    this.courses = this.content.getCourses()
   }
 
 }
