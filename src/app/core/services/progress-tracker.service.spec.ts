9b19b62abb4bc175b1d02bfaeb593c1478ece229
mfish33 <32677537+mfish33@users.noreply.github.com>
2020-06-24 22:10:14 -0400

    navbar color options progress tracker tests small spec file fixes

--- src/app/core/services/progress-tracker.service.spec.ts
+++ src/app/core/services/progress-tracker.service.spec.ts
@@ -1,12 +1,58 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ProgressTrackerService } from './progress-tracker.service';
+import { Course } from 'src/app/shared/models/contentfulTypes';
+
+const testCourse: Course = {
+  courseImage: {} as any,
+  courseTitle: 'Test Course',
+  courseDescription: {} as any,
+  lessons: ['', '', '', ''] as any
+}
+
+const testCourse2: Course = {
+  courseImage: {} as any,
+  courseTitle: 'Test Course2',
+  courseDescription: {} as any,
+  lessons: ['', '', '', ''] as any
+}
+
+const lids = ['12345', '67890']
 
 describe('ProgressTrackerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: ProgressTrackerService;
+  beforeEach(() => {
+    localStorage.removeItem('courseProgress')
+    service = new ProgressTrackerService()
+  });
 
   it('should be created', () => {
-    const service: ProgressTrackerService = TestBed.get(ProgressTrackerService);
     expect(service).toBeTruthy();
   });
+
+  it('should add a visited lesson', () => {
+    service.hasVisited(testCourse, lids[0])
+    console.log(service.progressReport())
+    expect(service.progressReport()).toEqual({
+      'Test Course': .25
+    })
+  })
+
+  it('Work with multiple courses', () => {
+    service.hasVisited(testCourse, lids[0])
+    service.hasVisited(testCourse, lids[1])
+    service.hasVisited(testCourse2, lids[0])
+    expect(service.progressReport()).toEqual({
+      'Test Course': .5,
+      'Test Course2': .25
+    })
+  })
+
+  it('should not double count already visited lessons', () => {
+    service.hasVisited(testCourse, lids[1])
+    service.hasVisited(testCourse, lids[1])
+    expect(service.progressReport()).toEqual({
+      'Test Course': .25
+    })
+  })
+
+
 });
