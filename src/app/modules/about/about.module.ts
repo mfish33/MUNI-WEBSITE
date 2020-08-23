bdf378dacddfbd3bfaec05924e5b81542c73513a
Max Fisher <mfish33>
2020-08-23 09:29:04 -0400

    about page UI initially done

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { MaterialModule } from 'src/app/shared/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [AboutPageComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AboutModule { }
--- src/app/modules/about/components/about-page/about-page.component.ts
+++ src/app/modules/about/components/about-page/about-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentfulService } from 'src/app/core/services/contentful.service';
 import { Profile } from 'src/app/shared/models/contentfulTypes';
+import {FormBuilder, FormGroup, FormArray } from '@angular/forms'
 import { AsyncPipe } from '@angular/common';
 
 
@@ -12,10 +13,17 @@ import { AsyncPipe } from '@angular/common';
 export class AboutPageComponent implements OnInit {
 
   personal$: Promise<Profile[]>
+  feedback:FormGroup
 
-  constructor(public content: ContentfulService) { }
+  constructor(public content: ContentfulService,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.feedback = this.fb.group({
+      name:'',
+      email:'',
+      reason:'',
+      body:''
+    })
     this.personal$ = this.content.getProfiles()
   }
 
