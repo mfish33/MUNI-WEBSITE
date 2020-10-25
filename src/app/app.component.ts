fa46e33e7eca4ef9481eaa72b5b19a29c9b43612
Max Fisher <maxmfishernj@gmail.com>
2020-10-25 10:27:20 -0700

    fix lesson and course hash to name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterHistoryService } from './core/services/router-history.service';
 import { AuthService } from './core/services/auth.service';
 import { ActivatedRoute } from "@angular/router";
+import { ContentfulService } from './core/services/contentful.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private history:RouterHistoryService, private auth:AuthService, private route: ActivatedRoute){}
+  // Pull in history and contentful services to get them to start at app launch
+  constructor(private history:RouterHistoryService, private auth:AuthService, private route: ActivatedRoute,private content:ContentfulService){}
   title = 'MUNI';
 
   ngOnInit() {
